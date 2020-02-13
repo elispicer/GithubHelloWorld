bc95ecdaf140bd3a5a9313a48c076ffa85fcfae3
Eli Spicer <joshua.spicer.2020@student.lhschools.org>
2020-02-13 11:00:50 -0500

    called Hello World Method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,12 +1,14 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        
+        helloWorld();
     }
 
-    public static void helloWorld(){
-        System.out.println("Hello World");
-    }
+    public static void helloWorld()
+
+        {System.out.println("Hello World");
+        }
+
 
 
 
